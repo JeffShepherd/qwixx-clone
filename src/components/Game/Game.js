0491c55cf878e96a382520eeb887c98368cd905d
Jeff S <jeff.shep777@gmail.com>
2022-06-27 12:44:21 -0600

    Add basic Game state and structure

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Game.css'
 
 function Game() {
+  const [turn, setTurn] = useState(1)
+  const scoreBoard = {
+    red: [2,3,4,5,6,7,8,9,10,11,12],
+    yellow: [2,3,4,5,6,7,8,9,10,11,12],
+    green: [12,11,10,9,8,7,6,5,4,3,2],
+    blue: [12,11,10,9,8,7,6,5,4,3,2]
+  }
+  const [player1Board, setPlayer1Board] = useState(scoreBoard)
+  const [player2Board, setPlayer2Board] = useState(scoreBoard)
+  const [gameMessage, setGameMessage] = useState('')
 
 
   return (
