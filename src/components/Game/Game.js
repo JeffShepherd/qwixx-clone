50bbcc09839ea942575bf3072cbb203b67a0d4a5
Jeff S <jeff.shep777@gmail.com>
2022-06-27 16:08:04 -0600

    Add and import Scoreboard component skeleton

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,25 +1,28 @@
 import React, { useState } from 'react'
 import './Game.css'
 import ActionBar from '../ActionBar/ActionBar'
+import Scoreboard from '../Scoreboard/Scoreboard'
 
 function Game() {
   const [turn, setTurn] = useState(1)
-  const scoreBoard = {
+  const gameBoard = {
     red: [2,3,4,5,6,7,8,9,10,11,12],
     yellow: [2,3,4,5,6,7,8,9,10,11,12],
     green: [12,11,10,9,8,7,6,5,4,3,2],
     blue: [12,11,10,9,8,7,6,5,4,3,2]
   }
-  const [player1Board, setPlayer1Board] = useState(scoreBoard)
-  const [player2Board, setPlayer2Board] = useState(scoreBoard)
+  const [player1Board, setPlayer1Board] = useState(gameBoard)
+  const [player2Board, setPlayer2Board] = useState(gameBoard)
   const [gameMessage, setGameMessage] = useState('Placeholder Message')
 
 
   return (
     <div className='game'>
+      <Scoreboard />
       <ActionBar 
       gameMessage={gameMessage}
       />
+      <Scoreboard />
     </div>
   )
 }
---
+++ src/components/Scoreboard/Scoreboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Scoreboard.css'
+
+function Scoreboard() {
+
+
+
+  return (
+    <section>
+
+
+      
+    </section>
+  )
+}
+
+export default Scoreboard
