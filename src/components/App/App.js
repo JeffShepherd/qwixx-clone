1ae2119ba8e02c66ee8405b7c0f88d6f4dcd54ec
Jeff S <jeff.shep777@gmail.com>
2022-06-27 12:10:08 -0600

    Add and import skeleton Game component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
+import React from 'react'
 import './App.css'
+import Game from '../Game/Game'
+
 
 function App() {
-
+  
   return (
     <div className="App">
-
+      <Game />
     </div>
   )
 }
---
+++ src/components/Game/Game.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Game.css'
+
+function Game() {
+
+
+  return (
+    <div className='game'>
+
+    </div>
+  )
+}
+
+export default Game
