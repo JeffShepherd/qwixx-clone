cc3f18cdf9ecc9a769c125ec33b762e5b5396578
Jeff S <jeff.shep777@gmail.com>
2022-07-25 15:12:33 -0600

    Add element to component per wireframes

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -5,7 +5,23 @@ function GameBoard() {
 
   return (
     <div className='gameboard'>
-
+      
+      <section className='gameboard-row'>
+        <p>2</p><p>3</p><p>4</p><p>5</p><p>6</p><p>7</p>
+        <p>8</p><p>9</p><p>10</p><p>11</p><p>12</p><p>lock</p>
+      </section>
+      <section className='gameboard-row'>
+        <p>2</p><p>3</p><p>4</p><p>5</p><p>6</p><p>7</p>
+        <p>8</p><p>9</p><p>10</p><p>11</p><p>12</p><p>lock</p>
+      </section>
+      <section className='gameboard-row'>
+        <p>12</p><p>11</p><p>10</p><p>9</p><p>8</p><p>7</p>
+        <p>6</p><p>5</p><p>4</p><p>3</p><p>2</p><p>lock</p>
+      </section>
+      <section className='gameboard-row'>
+        <p>12</p><p>11</p><p>10</p><p>9</p><p>8</p><p>7</p>
+        <p>6</p><p>5</p><p>4</p><p>3</p><p>2</p><p>lock</p>
+      </section>
       
     </div>
   )
